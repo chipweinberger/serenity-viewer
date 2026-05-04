67463a880cb62f196f7e84ac27c6cbb631d76c1a
Chip Weinberger <weinberger.c@gmail.com>
2026-05-05 00:24:44 +0800

    Refactor: move app header to provider composition

--- lib/src/app/header/app_header.dart
+++ lib/src/app/header/app_header.dart
@@ -1,31 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:serenity_viewer/src/app/header/app_tab_bar_actions.dart';
 import 'package:serenity_viewer/src/app/header/app_window_title.dart';
 import 'package:serenity_viewer/src/app/header/app_tab_bar.dart';
-import 'package:serenity_viewer/src/environment/workspace.dart';
+import 'package:serenity_viewer/src/app/controllers/app_ui_controller.dart';
+import 'package:serenity_viewer/src/app/state/app_derived_state.dart';
+import 'package:serenity_viewer/src/app/state/app_ui_handles.dart';
+import 'package:serenity_viewer/src/app/state/app_ui_state.dart';
+import 'package:serenity_viewer/src/environment/controller/environment_controller.dart';
+import 'package:serenity_viewer/src/environment/store/environment_store_state.dart';
 
 class AppHeader extends StatelessWidget {
-  const AppHeader({
-    super.key,
-    required this.windowTitle,
-    required this.openWorkspaces,
-    required this.activeWorkspaceId,
-    required this.isLibraryScreen,
-    required this.shouldMoveSelectedWindows,
-    required this.draggingTabWorkspaceId,
-    required this.tabScrollController,
-    required this.actions,
-  });
+  const AppHeader({super.key});
 
-  final String windowTitle;
-  final List<Workspace> openWorkspaces;
-  final String activeWorkspaceId;
-  final bool isLibraryScreen;
-  final bool shouldMoveSelectedWindows;
-  final String? draggingTabWorkspaceId;
-  final ScrollController tabScrollController;
-  final AppTabBarActions actions;
+  ({AppUiState appUiState, EnvironmentStoreState environmentStoreState}) _watchState(BuildContext context) {
+    return (appUiState: context.watch<AppUiState>(), environmentStoreState: context.watch<EnvironmentStoreState>());
+  }
+
+  ({AppUiHandles uiHandles, AppUiController appUiController, EnvironmentController environmentController})
+  _readDependencies(BuildContext context) {
+    return (
+      uiHandles: context.read<AppUiHandles>(),
+      appUiController: context.read<AppUiController>(),
+      environmentController: context.read<EnvironmentController>(),
+    );
+  }
 
   Widget _buildPointerShield() {
     return const Positioned(
@@ -37,34 +37,64 @@ class AppHeader extends StatelessWidget {
     );
   }
 
-  Widget _buildTabBar() {
+  Widget _buildTabBarFromState({
+    required AppUiState appUiState,
+    required EnvironmentStoreState environmentStoreState,
+    required AppUiHandles uiHandles,
+    required AppUiController appUiController,
+    required EnvironmentController environmentController,
+  }) {
+    final environment = environmentStoreState.environment!;
     return Positioned(
       left: 18,
       right: 18,
       top: 28,
       child: AppTabBar(
-        openWorkspaces: openWorkspaces,
-        activeWorkspaceId: activeWorkspaceId,
-        isLibraryScreen: isLibraryScreen,
-        shouldMoveSelectedWindows: shouldMoveSelectedWindows,
-        draggingTabWorkspaceId: draggingTabWorkspaceId,
-        tabScrollController: tabScrollController,
-        actions: actions,
+        openWorkspaces: deriveOpenWorkspaces(environmentStoreState),
+        activeWorkspaceId: environment.activeWorkspaceId,
+        isLibraryScreen: appUiController.isLibraryScreen,
+        shouldMoveSelectedWindows: appUiController.shouldMoveSelectedWindowsToWorkspaceOnTap,
+        draggingTabWorkspaceId: appUiState.draggingTabWorkspaceId,
+        tabScrollController: uiHandles.tabScrollController,
+        actions: AppTabBarActions(
+          onShowWorkspaceOverview: environmentController.navigation.showOverview,
+          onSetDraggingTabWorkspaceId: appUiController.setDraggingTabWorkspaceId,
+          onReorderOpenWorkspace: environmentController.management.reorderOpen,
+          onMoveSelectedExposeWindowsToWorkspace: environmentController.management.moveSelectedExposeWindowsToWorkspace,
+          onSetActiveWorkspace: environmentController.navigation.setActiveWorkspace,
+          onConfirmCloseTab: environmentController.management.confirmCloseTab,
+          onCreateWorkspace: environmentController.management.create,
+        ),
       ),
     );
   }
 
-  Widget _buildTitle() {
+  Widget _buildTitle(EnvironmentStoreState environmentStoreState) {
     return Positioned(
       top: 10,
       left: 120,
       right: 120,
-      child: Center(child: AppWindowTitle(windowTitle: windowTitle)),
+      child: Center(child: AppWindowTitle(windowTitle: deriveWindowTitle(environmentStoreState))),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [_buildPointerShield(), _buildTabBar(), _buildTitle()]);
+    final state = _watchState(context);
+    final dependencies = _readDependencies(context);
+
+    return Stack(
+      children: [
+        _buildPointerShield(),
+        _buildTabBarFromState(
+          appUiState: state.appUiState,
+          environmentStoreState: state.environmentStoreState,
+          uiHandles: dependencies.uiHandles,
+          appUiController: dependencies.appUiController,
+          environmentController: dependencies.environmentController,
+        ),
+        _buildTitle(state.environmentStoreState),
+      ],
+    );
   }
 }
--- lib/src/app/views/app_main_view.dart
+++ lib/src/app/views/app_main_view.dart
@@ -2,31 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:serenity_viewer/src/app/header/app_header.dart';
-import 'package:serenity_viewer/src/app/header/app_tab_bar_actions.dart';
-import 'package:serenity_viewer/src/app/controllers/app_ui_controller.dart';
-import 'package:serenity_viewer/src/app/state/app_derived_state.dart';
-import 'package:serenity_viewer/src/app/state/app_ui_handles.dart';
 import 'package:serenity_viewer/src/app/state/app_ui_state.dart';
-import 'package:serenity_viewer/src/environment/store/environment_store_state.dart';
 import 'package:serenity_viewer/src/app/views/library_view.dart';
 import 'package:serenity_viewer/src/app/views/workspace_view.dart';
-import 'package:serenity_viewer/src/environment/controller/environment_controller.dart';
 import 'package:serenity_viewer/src/foundation/app_constants.dart';
 
 class AppMainView extends StatelessWidget {
   const AppMainView({super.key});
 
-  ({AppUiState appUiState, EnvironmentStoreState environmentStoreState}) _watchState(BuildContext context) {
-    return (appUiState: context.watch<AppUiState>(), environmentStoreState: context.watch<EnvironmentStoreState>());
-  }
-
-  ({AppUiHandles uiHandles, AppUiController appUiController, EnvironmentController environmentController})
-  _readDependencies(BuildContext context) {
-    return (
-      uiHandles: context.read<AppUiHandles>(),
-      appUiController: context.read<AppUiController>(),
-      environmentController: context.read<EnvironmentController>(),
-    );
+  AppUiState _watchState(BuildContext context) {
+    return context.watch<AppUiState>();
   }
 
   int _activeScreenIndex(AppUiState appUiState) {
@@ -36,54 +21,19 @@ class AppMainView extends StatelessWidget {
     };
   }
 
-  Widget _buildWorkspaceUiOverlay({
-    required AppUiState appUiState,
-    required EnvironmentStoreState environmentStoreState,
-    required AppUiController appUiController,
-    required EnvironmentController environmentController,
-    required AppUiHandles uiHandles,
-  }) {
-    final environment = environmentStoreState.environment!;
-    return AppHeader(
-      windowTitle: deriveWindowTitle(environmentStoreState),
-      openWorkspaces: deriveOpenWorkspaces(environmentStoreState),
-      activeWorkspaceId: environment.activeWorkspaceId,
-      isLibraryScreen: appUiController.isLibraryScreen,
-      shouldMoveSelectedWindows: appUiController.shouldMoveSelectedWindowsToWorkspaceOnTap,
-      draggingTabWorkspaceId: appUiState.draggingTabWorkspaceId,
-      tabScrollController: uiHandles.tabScrollController,
-      actions: AppTabBarActions(
-        onShowWorkspaceOverview: environmentController.navigation.showOverview,
-        onSetDraggingTabWorkspaceId: appUiController.setDraggingTabWorkspaceId,
-        onReorderOpenWorkspace: environmentController.management.reorderOpen,
-        onMoveSelectedExposeWindowsToWorkspace: environmentController.management.moveSelectedExposeWindowsToWorkspace,
-        onSetActiveWorkspace: environmentController.navigation.setActiveWorkspace,
-        onConfirmCloseTab: environmentController.management.confirmCloseTab,
-        onCreateWorkspace: environmentController.management.create,
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    final state = _watchState(context);
-    final dependencies = _readDependencies(context);
+    final appUiState = _watchState(context);
 
     return Stack(
       children: [
         Positioned.fill(
           child: IndexedStack(
-            index: _activeScreenIndex(state.appUiState),
+            index: _activeScreenIndex(appUiState),
             children: [const WorkspaceView(), const LibraryView()],
           ),
         ),
-        _buildWorkspaceUiOverlay(
-          appUiState: state.appUiState,
-          environmentStoreState: state.environmentStoreState,
-          appUiController: dependencies.appUiController,
-          environmentController: dependencies.environmentController,
-          uiHandles: dependencies.uiHandles,
-        ),
+        const AppHeader(),
       ],
     );
   }
