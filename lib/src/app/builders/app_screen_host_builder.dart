ae2e593966a402eb0b1e56d3246be6af74ad7e8c
Chip Weinberger <weinberger.c@gmail.com>
2026-05-04 10:44:39 +0800

    Refactor: inline workspace top bar overlay builder

--- lib/src/app/builders/app_screen_host_builder.dart
+++ lib/src/app/builders/app_screen_host_builder.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-import 'package:serenity_viewer/src/app/builders/app_top_bar_overlay_builder.dart';
 import 'package:serenity_viewer/src/app/builders/app_screen_host_scope.dart';
 import 'package:serenity_viewer/src/app/builders/library_content_builder.dart';
 import 'package:serenity_viewer/src/app/builders/workspace_content_builder.dart';
 import 'package:serenity_viewer/src/foundation/app_constants.dart';
 import 'package:serenity_viewer/src/media/loading/media_load_plan.dart';
 import 'package:serenity_viewer/src/media/loading/workspace_load_plan.dart';
+import 'package:serenity_viewer/src/workspace/screen/workspace_top_bar_overlay.dart';
 
 class AppScreenHostBuilder {
   const AppScreenHostBuilder({required this.state, required this.actions});
@@ -53,6 +53,24 @@ class AppScreenHostBuilder {
   }
 
   Widget _buildWorkspaceUiOverlay() {
-    return AppTopBarOverlayBuilder(state: state).build();
+    return WorkspaceTopBarOverlay(
+      windowTitle: state.windowTitle,
+      openWorkspaces: state.openWorkspaces,
+      activeWorkspaceId: state.environment.activeWorkspaceId,
+      isLibraryScreen: state.appUiController.isLibraryScreen,
+      shouldMoveSelectedWindows: state.appUiController.shouldMoveSelectedWindowsToWorkspaceOnTap,
+      draggingTabWorkspaceId: state.uiState.draggingTabWorkspaceId,
+      tabScrollController: state.tabScrollController,
+      actions: WorkspaceTopBarOverlayActions(
+        onShowWorkspaceOverview: state.environmentSession.navigation.showOverview,
+        onSetDraggingTabWorkspaceId: state.appUiController.setDraggingTabWorkspaceId,
+        onReorderOpenWorkspace: state.environmentSession.management.reorderOpen,
+        onMoveSelectedExposeWindowsToWorkspace:
+            state.environmentSession.management.moveSelectedExposeWindowsToWorkspace,
+        onSetActiveWorkspace: state.environmentSession.navigation.setActiveWorkspace,
+        onConfirmCloseTab: state.environmentSession.management.confirmCloseTab,
+        onCreateWorkspace: state.environmentSession.management.create,
+      ),
+    );
   }
 }
