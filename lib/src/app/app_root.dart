dde44fe7de3e73e343197e00ab56fc192e028522
Chip Weinberger <weinberger.c@gmail.com>
2026-05-05 00:08:50 +0800

    Refactor: inline app provider composition in app root

--- lib/src/app/app_root.dart
+++ lib/src/app/app_root.dart
@@ -1,25 +1,45 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import 'package:serenity_viewer/src/app/app_providers.dart';
+import 'package:serenity_viewer/src/app/controllers/app_feedback_controller.dart';
+import 'package:serenity_viewer/src/app/controllers/app_ui_controller.dart';
+import 'package:serenity_viewer/src/app/platform/platform_bridge.dart';
 import 'package:serenity_viewer/src/app/runtime/app_runtime.dart';
 import 'package:serenity_viewer/src/app/app_shell.dart';
 import 'package:serenity_viewer/src/app/state/app_ui_handles.dart';
 import 'package:serenity_viewer/src/app/state/app_ui_state.dart';
-import 'package:serenity_viewer/src/app/state/app_derived_state.dart';
-import 'package:serenity_viewer/src/app/controllers/app_feedback_controller.dart';
 import 'package:serenity_viewer/src/app/seed_environment.dart';
+import 'package:serenity_viewer/src/app/state/app_derived_state.dart';
 import 'package:serenity_viewer/src/environment/document/document_persistence_controller.dart';
+import 'package:serenity_viewer/src/environment/document/document_coordinator.dart';
+import 'package:serenity_viewer/src/environment/controller/environment_controller.dart';
+import 'package:serenity_viewer/src/environment/store/environment_store.dart';
 import 'package:serenity_viewer/src/environment/store/environment_store_state.dart';
-import 'package:serenity_viewer/src/settings/behavior/app_settings_controller.dart';
 import 'package:serenity_viewer/src/foundation/app_constants.dart';
 import 'package:serenity_viewer/src/foundation/serenity_identity.dart';
+import 'package:serenity_viewer/src/media/import/workspace_media_import_controller.dart';
+import 'package:serenity_viewer/src/media/video/shared_video_controller_pool.dart';
+import 'package:serenity_viewer/src/settings/behavior/app_settings_controller.dart';
 import 'package:serenity_viewer/src/window/interaction/window_interaction_state.dart';
+import 'package:serenity_viewer/src/workspace/actions/workspace_asset_picker_controller.dart';
+import 'package:serenity_viewer/src/workspace/actions/workspace_collate_controller.dart';
+import 'package:serenity_viewer/src/workspace/actions/workspace_expose_layout_controller.dart';
+import 'package:serenity_viewer/src/workspace/actions/workspace_video_conversion_controller.dart';
+import 'package:serenity_viewer/src/workspace/controllers/workspace_controller.dart';
 import 'package:serenity_viewer/src/workspace/history/workspace_window_history_state.dart';
+import 'package:serenity_viewer/src/workspace/history/workspace_window_history_controller.dart';
+import 'package:serenity_viewer/src/workspace/input/workspace_shortcut_controller.dart';
+import 'package:serenity_viewer/src/workspace/links/workspace_links_controller.dart';
+import 'package:serenity_viewer/src/workspace/links/workspace_links_launcher.dart';
+import 'package:serenity_viewer/src/workspace/links/workspace_links_prompts.dart';
 import 'package:serenity_viewer/src/workspace/thumbnails/thumbnail_refresh_state.dart';
+import 'package:serenity_viewer/src/workspace/thumbnails/thumbnail_controller.dart';
 import 'package:serenity_viewer/src/workspace/tracking/workspace_view_tracking_state.dart';
 import 'package:serenity_viewer/src/workspace/viewport/workspace_viewport_state.dart';
+import 'package:serenity_viewer/src/workspace/controllers/workspace_viewport_session_controller.dart';
+import 'package:serenity_viewer/src/workspace/controllers/workspace_window_controller.dart';
 
 class AppRoot extends StatefulWidget {
   const AppRoot({super.key});
@@ -147,18 +167,39 @@ class _AppRootState extends State<AppRoot> {
 
   @override
   Widget build(BuildContext context) {
-    return AppProviders(
-      appUiState: _appUiState,
-      environmentStoreState: _environmentStoreState,
-      windowInteractionState: _windowInteractionState,
-      workspaceViewportState: _workspaceViewportState,
-      thumbnailRefreshState: _thumbnailRefreshState,
-      workspaceWindowHistoryState: _workspaceWindowHistoryState,
-      workspaceViewTrackingState: _workspaceViewTrackingState,
-      uiHandles: _uiHandles,
-      feedback: _feedback,
-      settings: _settings,
-      runtime: _runtime,
+    return MultiProvider(
+      providers: [
+        Provider<AppUiHandles>.value(value: _uiHandles),
+        Provider<AppFeedbackController>.value(value: _feedback),
+        Provider<AppSettingsController>.value(value: _settings),
+        ChangeNotifierProvider.value(value: _appUiState),
+        ChangeNotifierProvider.value(value: _environmentStoreState),
+        ChangeNotifierProvider.value(value: _windowInteractionState),
+        ChangeNotifierProvider.value(value: _workspaceViewportState),
+        ChangeNotifierProvider.value(value: _thumbnailRefreshState),
+        ChangeNotifierProvider.value(value: _workspaceWindowHistoryState),
+        ChangeNotifierProvider.value(value: _workspaceViewTrackingState),
+        Provider<AppUiController>.value(value: _runtime.appUiController),
+        Provider<SharedVideoControllerPool>.value(value: _runtime.sharedVideoControllerPool),
+        Provider<PlatformBridge>.value(value: _runtime.platformBridge),
+        Provider<EnvironmentStore>.value(value: _runtime.environmentStore),
+        Provider<DocumentCoordinator>.value(value: _runtime.documentCoordinator),
+        Provider<WorkspaceController>.value(value: _runtime.workspaceController),
+        Provider<EnvironmentController>.value(value: _runtime.environmentController),
+        Provider<WorkspaceExposeLayoutController>.value(value: _runtime.workspaceExposeLayoutController),
+        Provider<WorkspaceLinksController>.value(value: _runtime.workspaceLinksController),
+        Provider<WorkspaceLinksLauncher>.value(value: _runtime.workspaceLinksLauncher),
+        Provider<WorkspaceLinksPrompts>.value(value: _runtime.workspaceLinksPrompts),
+        Provider<ThumbnailController>.value(value: _runtime.thumbnailController),
+        Provider<WorkspaceWindowHistoryController>.value(value: _runtime.workspaceWindowHistoryController),
+        Provider<WorkspaceMediaImportController>.value(value: _runtime.workspaceMediaImportController),
+        Provider<WorkspaceWindowController>.value(value: _runtime.workspaceWindowController),
+        Provider<WorkspaceViewportSessionController>.value(value: _runtime.workspaceViewportSessionController),
+        Provider<WorkspaceCollateController>.value(value: _runtime.workspaceCollateController),
+        Provider<WorkspaceVideoConversionController>.value(value: _runtime.workspaceVideoConversionController),
+        Provider<WorkspaceAssetPickerController>.value(value: _runtime.workspaceAssetPickerController),
+        Provider<WorkspaceShortcutController>.value(value: _runtime.workspaceShortcutController),
+      ],
       child: const AppShell(),
     );
   }
